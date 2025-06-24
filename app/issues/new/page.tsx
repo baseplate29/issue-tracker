06ee4b7105e7dfb44df7d5b0dce217681919ef20
rjbaes <rjbaes@gmail.com>
2025-06-24 20:27:02 +0800

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,25 +1,32 @@
 'use client';
 
-import { Button, Callout, TextField } from '@radix-ui/themes';
-import React, { useState } from 'react';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
+import React, { CSSProperties, useState } from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 import { useForm, Controller } from 'react-hook-form';
 import 'easymde/dist/easymde.min.css';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState('');
 
   return (
-    <div className="max-w-xl ">
+    <div className="max-w-xl">
       {error && (
         <Callout.Root color="red" className="mb-5">
           <Callout.Text>{error}</Callout.Text>
@@ -41,6 +48,11 @@ const NewIssuePage = () => {
           autoComplete="off"
           {...register('title')}
         />
+        {errors.title && (
+          <Text color="red" as="p" style={textStyle}>
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -48,10 +60,17 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p" style={textStyle}>
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
   );
 };
+
+const textStyle: CSSProperties = { marginBottom: '10px' };
 
 export default NewIssuePage;
