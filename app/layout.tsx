ee68000663cc32326fa571536bc269e876b5fd99
rjbaes <rjbaes@gmail.com>
2025-06-24 17:36:17 +0800

    Customize Radix UI Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,24 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
-import NavBar from './NavBar';
 import '@radix-ui/themes/styles.css';
+import './theme-config.css';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import NavBar from './NavBar';
 import { Theme } from '@radix-ui/themes';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
+// const geistSans = Geist({
+//   variable: '--font-geist-sans',
+//   subsets: ['latin'],
+// });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+// const geistMono = Geist_Mono({
+//   variable: '--font-geist-mono',
+//   subsets: ['latin'],
+// });
+
+const inter = Inter({
   subsets: ['latin'],
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -27,12 +33,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Theme>
+      <body className={`${inter.variable} antialiased`}>
+        <Theme accentColor="violet">
           <NavBar />
-          <main className='p-5'>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
