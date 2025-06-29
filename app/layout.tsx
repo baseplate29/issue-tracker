6bb1261f2cf5328caa7a013744fd39640ba87988
rjbaes <rjbaes@gmail.com>
2025-06-30 01:26:26 +0800

    add a delete button

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import NavBar from './NavBar';
-import { Theme } from '@radix-ui/themes';
+import { Container, Theme } from '@radix-ui/themes';
 
 // const geistSans = Geist({
 //   variable: '--font-geist-sans',
@@ -36,7 +36,9 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased`}>
         <Theme accentColor="violet">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
