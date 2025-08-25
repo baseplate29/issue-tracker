ba1600b0ae999ef53dd6a4aaaec8839de3007988
rjbaes <rjbaes@gmail.com>
2025-08-25 16:27:34 +0800

    build the IssueSummary component

--- app/issues/list/IssueTable.tsx
+++ app/issues/list/IssueTable.tsx
@@ -1,71 +1,71 @@
-import { IssueStatusBadge, Link } from '@/app/components'
+import { IssueStatusBadge, Link } from '@/app/components';
 import { Issue, Status } from '@prisma/client';
-import { ArrowUpIcon } from '@radix-ui/react-icons'
-import { Table } from '@radix-ui/themes'
+import { ArrowUpIcon } from '@radix-ui/react-icons';
+import { Table } from '@radix-ui/themes';
 import NextLink from 'next/link';
-import React from 'react'
+import React from 'react';
 
 export interface IssueQuery {
-    status: Status; orderBy: keyof Issue, page: string
+  status: Status;
+  orderBy: keyof Issue;
+  page: string;
 }
 
 interface Props {
-    searchParams: Promise<IssueQuery>,
-    issues: Issue[];
+  searchParams: Promise<IssueQuery>;
+  issues: Issue[];
 }
 
 const IssueTable = async ({ searchParams, issues }: Props) => {
-    const params = await searchParams;
+  const params = await searchParams;
 
-    return (
-        <Table.Root variant="surface">
-            <Table.Header>
-                <Table.Row>
-                    {columns.map((column) => (
-                        <Table.ColumnHeaderCell
-                            key={column.value}
-                            className={column.className}
-                        >
-                            <NextLink
-                                href={{ query: { ...params, orderBy: column.value } }}
-                            >
-                                {column.label}
-                            </NextLink>
-                            {column.value === params.orderBy && (
-                                <ArrowUpIcon className="inline" />
-                            )}
-                        </Table.ColumnHeaderCell>
-                    ))}
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {issues.map((issue) => (
-                    <Table.Row key={issue.id}>
-                        <Table.Cell>
-                            <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                            <div className="block md:hidden">
-                                <IssueStatusBadge status={issue.status} />
-                            </div>
-                        </Table.Cell>
-                        <Table.Cell className="hidden md:table-cell">
-                            <IssueStatusBadge status={issue.status} />
-                        </Table.Cell>
-                        <Table.Cell className="hidden md:table-cell">
-                            {issue.createdAt.toDateString()}
-                        </Table.Cell>
-                    </Table.Row>
-                ))}
-            </Table.Body>
-        </Table.Root>
-    )
-}
+  return (
+    <Table.Root variant="surface">
+      <Table.Header>
+        <Table.Row>
+          {columns.map((column) => (
+            <Table.ColumnHeaderCell
+              key={column.value}
+              className={column.className}
+            >
+              <NextLink href={{ query: { ...params, orderBy: column.value } }}>
+                {column.label}
+              </NextLink>
+              {column.value === params.orderBy && (
+                <ArrowUpIcon className="inline" />
+              )}
+            </Table.ColumnHeaderCell>
+          ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {issues.map((issue) => (
+          <Table.Row key={issue.id}>
+            <Table.Cell>
+              <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+              <div className="block md:hidden">
+                <IssueStatusBadge status={issue.status} />
+              </div>
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              <IssueStatusBadge status={issue.status} />
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              {issue.createdAt.toDateString()}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+};
 
 const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: 'Issue', value: 'title' },
-    { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
-    { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' },
+  { label: 'Issue', value: 'title' },
+  { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
+  { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' },
 ];
 
 export const columnNames = columns.map((column) => column.value);
 
-export default IssueTable
+export default IssueTable;
